5d78dc9ce28c96e5b71e180e11e753a5998147b3
John Vincent Valena <valena.john.vincent@gmail.com>
2023-03-08 20:55:03 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  devServer: {
+    port: 3000,
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "blog.js",
+    clean: true,
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+};
