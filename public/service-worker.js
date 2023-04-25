b63f97128c9773ba4316c85eeb41118837c8ba2e
John Vincent Valena <valena.john.vincent@gmail.com>
2023-04-26 00:44:37 +0800

    Service worker changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,42 +1,37 @@
-let CACHE_NAME = "expense-tracker";
-let urlsToCache = ["/"];
+const cacheName = "expense-tracker";
+const filesToCache = ["index.html"];
 
-// Install a service worker
-self.addEventListener("install", (event) => {
+self.addEventListener("install", function(event) {
   // Perform install steps
+  console.log("[Servicework] Install");
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
+    caches.open(cacheName).then(function(cache) {
+      console.log("[ServiceWorker] Caching app shell");
+      return cache.addAll(filesToCache);
     })
   );
 });
 
-// Cache and return requests
+self.addEventListener("activate", function(event) {
+  console.log("[Servicework] Activate");
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log("[ServiceWorker] Removing old cache shell", key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
 self.addEventListener("fetch", (event) => {
+  console.log("[ServiceWorker] Fetch");
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
-});
 
-// Update a service worker
-self.addEventListener("activate", (event) => {
-  let cacheWhitelist = ["expense-tracker"];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
 });
